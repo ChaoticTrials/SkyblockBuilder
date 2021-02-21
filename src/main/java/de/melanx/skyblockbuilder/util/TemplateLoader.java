27f6e2389b6d78214ab8168467c3a42424625333
MelanX <info@melanx.de>
2021-02-21 14:36:33 +0100

    allow schematic being available in config

--- src/main/java/de/melanx/skyblockbuilder/util/TemplateLoader.java
+++ src/main/java/de/melanx/skyblockbuilder/util/TemplateLoader.java
@@ -7,16 +7,29 @@ import net.minecraft.resources.IResource;
 import net.minecraft.resources.IResourceManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.gen.feature.template.Template;
+import net.minecraftforge.fml.loading.FMLPaths;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class TemplateLoader {
-    private static final ResourceLocation ID = new ResourceLocation(SkyblockBuilder.MODID, "structures/custom_skyblock.nbt");
+    private static final ResourceLocation ID = new ResourceLocation(SkyblockBuilder.MODID, "skyblock_builder.nbt");
+    private static final Path PATH = FMLPaths.CONFIGDIR.get().resolve("skyblock_builder.nbt");
     public static final Template TEMPLATE = new Template();
 
     public static void loadSchematic(IResourceManager manager) throws IOException {
-        IResource file = manager.getResource(ID);
-        CompoundNBT nbt = CompressedStreamTools.readCompressed(file.getInputStream());
+        File file = new File(PATH.toUri());
+        CompoundNBT nbt;
+
+        if (file.exists()) {
+            nbt = CompressedStreamTools.readCompressed(new FileInputStream(file));
+        } else {
+            IResource resource = manager.getResource(ID);
+            nbt = CompressedStreamTools.readCompressed(resource.getInputStream());
+        }
+
         TEMPLATE.read(nbt);
     }
 }
