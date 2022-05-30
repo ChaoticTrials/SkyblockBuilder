9b871689711d87fbb7f4ac6bda9e12baeb6705f6
MelanX <info@melanx.de>
2022-05-30 19:23:58 +0200

    add allowed biomes if no existing biome in this dimension type matches the lists entries
    closes #80

--- src/main/java/de/melanx/skyblockbuilder/util/BiomeSourceConverter.java
+++ src/main/java/de/melanx/skyblockbuilder/util/BiomeSourceConverter.java
@@ -15,31 +15,57 @@ import net.minecraft.util.GsonHelper;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.BiomeSource;
 import net.minecraft.world.level.biome.MultiNoiseBiomeSource;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.Optional;
 
 public class BiomeSourceConverter {
 
-    public static BiomeSource customBiomeSource(ResourceKey<Level> level, RegistryAccess registryAccess, BiomeSource source) {
+    public static BiomeSource customBiomeSource(ResourceKey<Level> level, RegistryAccess registryAccess, BiomeSource baseSource) {
         RegistryOps<JsonElement> dynOps = RegistryOps.create(JsonOps.INSTANCE, registryAccess);
-        if (source instanceof MultiNoiseBiomeSource noiseBiomeSource) {
-            if (noiseBiomeSource.preset.isPresent()) {
-                MultiNoiseBiomeSource.PresetInstance presetInstance = noiseBiomeSource.preset.get();
-                source = presetInstance.preset().biomeSource(presetInstance, false);
-            }
+        BiomeSource modifiableSource;
+        if (baseSource instanceof MultiNoiseBiomeSource noiseBiomeSource && noiseBiomeSource.preset.isPresent()) {
+            MultiNoiseBiomeSource.PresetInstance presetInstance = noiseBiomeSource.preset.get();
+            modifiableSource = presetInstance.preset().biomeSource(presetInstance, false);
+        } else {
+            modifiableSource = baseSource;
         }
 
-        Optional<JsonElement> sourceElement = BiomeSource.CODEC.encodeStart(dynOps, source).result();
+        Optional<JsonElement> sourceElement = BiomeSource.CODEC.encodeStart(dynOps, modifiableSource).result();
         if (sourceElement.isPresent()) {
+            ResourceList resourceList = ConfigHandler.World.biomes.get(level.location().toString());
             JsonObject json = sourceElement.get().getAsJsonObject();
             JsonArray biomes = GsonHelper.getAsJsonArray(json, "biomes", new JsonArray());
             JsonArray newBiomes = new JsonArray();
             for (JsonElement biome : biomes) {
                 ResourceLocation location = new ResourceLocation(GsonHelper.getAsString(biome.getAsJsonObject(), "biome"));
-                ResourceList resourceList = ConfigHandler.World.biomes.get(level.location().toString());
                 if (resourceList.test(location)) {
                     newBiomes.add(biome);
                 }
+            }
+
+            if (newBiomes.isEmpty()) {
+                ForgeRegistries.BIOMES.getEntries().forEach(entry -> {
+                    if (resourceList.test(entry.getKey().location())) {
+                        JsonObject biomeEntry = new JsonObject();
+                        JsonObject parameters = new JsonObject();
+                        JsonArray emptyArray = new JsonArray();
+                        emptyArray.add(0);
+                        emptyArray.add(0);
+                        parameters.addProperty("erosion", 0.0);
+                        parameters.addProperty("depth", 0.0);
+                        parameters.addProperty("weirdness", 0.0);
+                        parameters.addProperty("offset", 0.0);
+                        parameters.addProperty("temperature", 0.0);
+                        parameters.addProperty("humidity", 0.0);
+                        parameters.addProperty("continentalness", 0.0);
+                        biomeEntry.add("parameters", parameters);
+                        //noinspection ConstantConditions
+                        biomeEntry.addProperty("biome", entry.getValue().getRegistryName().toString());
+
+                        newBiomes.add(biomeEntry);
+                    }
+                });
             }
 
             json.add("biomes", newBiomes);
@@ -51,6 +77,6 @@ public class BiomeSourceConverter {
             }
         }
 
-        return source;
+        return baseSource;
     }
 }
