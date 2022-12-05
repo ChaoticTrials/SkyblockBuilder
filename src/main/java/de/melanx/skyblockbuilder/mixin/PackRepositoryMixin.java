84a1d5826c0141df7b3fd164ebc1eeb5d9446104
MelanX <info@melanx.de>
2022-12-05 19:21:21 +0100

    fix datapack order when vanilla datapack is enabled
    closes #108

---
+++ src/main/java/de/melanx/skyblockbuilder/mixin/PackRepositoryMixin.java
@@ -0,0 +1,28 @@
+package de.melanx.skyblockbuilder.mixin;
+
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackRepository;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+@Mixin(PackRepository.class)
+public class PackRepositoryMixin {
+
+    @Inject(method = "rebuildSelected", at = @At(value = "RETURN"), cancellable = true)
+    private void reorderIds(Collection<String> ids, CallbackInfoReturnable<List<Pack>> cir) {
+        List<Pack> list = new ArrayList<>(cir.getReturnValue());
+        Optional<Pack> vanilla = list.stream().filter(entry -> entry.getId().equals("vanilla")).findAny();
+        if (vanilla.isPresent()) {
+            list.remove(vanilla.get());
+            list.add(0, vanilla.get());
+            cir.setReturnValue(List.copyOf(list));
+        }
+    }
+}
