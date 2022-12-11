b5e561fbe3a27a08560f90b23bebb4775fa953d7
MelanX <info@melanx.de>
2022-12-11 20:34:16 +0100

    fix outer end generation
    closes #110

--- src/main/java/de/melanx/skyblockbuilder/world/chunkgenerators/SkyblockEndChunkGenerator.java
+++ src/main/java/de/melanx/skyblockbuilder/world/chunkgenerators/SkyblockEndChunkGenerator.java
@@ -9,6 +9,7 @@ import net.minecraft.core.Registry;
 import net.minecraft.resources.RegistryOps;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.WorldGenRegion;
+import net.minecraft.util.Mth;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.StructureFeatureManager;
@@ -17,7 +18,8 @@ import net.minecraft.world.level.biome.BiomeSource;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.chunk.ChunkGenerator;
-import net.minecraft.world.level.levelgen.*;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
 import net.minecraft.world.level.levelgen.blending.Blender;
 import net.minecraft.world.level.levelgen.structure.StructureSet;
 import net.minecraft.world.level.levelgen.synth.NormalNoise;
@@ -27,6 +29,8 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
 public class SkyblockEndChunkGenerator extends SkyblockNoiseBasedChunkGenerator {
+
+    private static final int MAIN_ISLAND_DISTANCE = 16;
 
     // [VanillaCopy] overworld chunk generator codec
     public static final Codec<SkyblockEndChunkGenerator> CODEC = RecordCodecBuilder.create(
@@ -59,19 +63,6 @@ public class SkyblockEndChunkGenerator extends SkyblockNoiseBasedChunkGenerator 
     public void buildSurface(@Nonnull WorldGenRegion level, @Nonnull StructureFeatureManager structureManager, @Nonnull ChunkAccess chunk) {
         super.buildSurface(level, structureManager, chunk);
 
-        if (ConfigHandler.Dimensions.End.mainIsland) {
-            // [Vanilla copy] to use registry wrapper when calling SurfaceSystem#buildSurface
-            WorldGenerationContext worldGenerationContext = new WorldGenerationContext(this, level);
-            NoiseGeneratorSettings noiseGeneratorSettings = this.generatorSettings.value();
-            NoiseChunk noisechunk = chunk.getOrCreateNoiseChunk(this.router(), () -> {
-                //noinspection CodeBlock2Expr
-                return new Beardifier(structureManager, chunk);
-            }, noiseGeneratorSettings, this.globalFluidPicker, Blender.of(level));
-            this.surfaceSystem.buildSurface(level.getBiomeManager(), level.registryAccess().registryOrThrow(Registry.BIOME_REGISTRY),
-                    noiseGeneratorSettings.useLegacyRandomSource(), worldGenerationContext, chunk, noisechunk, noiseGeneratorSettings.surfaceRule());
-            return;
-        }
-
         ChunkPos chunkPos = chunk.getPos();
         if (chunkPos.x == 0 && chunkPos.z == 0) {
             chunk.setBlockState(new BlockPos(0, 64, 0), Blocks.BEDROCK.defaultBlockState(), false);
@@ -81,7 +72,8 @@ public class SkyblockEndChunkGenerator extends SkyblockNoiseBasedChunkGenerator 
     @Nonnull
     @Override
     public CompletableFuture<ChunkAccess> fillFromNoise(@Nonnull Executor executor, @Nonnull Blender blender, @Nonnull StructureFeatureManager manager, @Nonnull ChunkAccess chunk) {
-        if (ConfigHandler.Dimensions.End.mainIsland) {
+        ChunkPos chunkPos = chunk.getPos();
+        if (ConfigHandler.Dimensions.End.mainIsland && Mth.abs(chunkPos.x) <= MAIN_ISLAND_DISTANCE && Mth.abs(chunkPos.z) <= MAIN_ISLAND_DISTANCE) {
             return this.parent.fillFromNoise(executor, blender, manager, chunk);
         }
 
@@ -91,8 +83,8 @@ public class SkyblockEndChunkGenerator extends SkyblockNoiseBasedChunkGenerator 
     @Override
     public void applyCarvers(@Nonnull WorldGenRegion level, long seed, @Nonnull BiomeManager biomeManager, @Nonnull StructureFeatureManager structureManager, @Nonnull ChunkAccess chunk, @Nonnull GenerationStep.Carving carving) {
         ChunkPos pos = chunk.getPos();
-        int value = 10 * 16;
-        if (pos.getMinBlockX() < value && pos.getMinBlockX() > -value && pos.getMinBlockZ() < value && pos.getMinBlockZ() > -value) {
+        int value = MAIN_ISLAND_DISTANCE * 16;
+        if (pos.getMinBlockX() <= value && pos.getMinBlockX() >= -value && pos.getMinBlockZ() <= value && pos.getMinBlockZ() >= -value) {
             super.applyCarvers(level, seed, biomeManager, structureManager, chunk, carving);
         }
     }
