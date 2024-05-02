c61bd402b993986b834db79458ff9b0a084aa712
MelanX <info@melanx.de>
2024-05-02 18:40:12 +0200

    clean up coremods

--- src/coremods/coremods/exit_portal.js
+++ src/coremods/coremods/exit_portal.js
@@ -1,44 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", {value: true});
 var coremods_1 = require("coremods");
-// function initializeCoreMod(): CoreMods {
-//     return {
-//         'redirect_exit_portal': {
-//             'target': {
-//                 'type': 'METHOD',
-//                 'class': 'net.minecraft.server.level.ServerPlayer',
-//                 'methodName': 'm_183318_',
-//                 'methodDesc': '(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;ZLnet/minecraft/world/level/border/WorldBorder;)Ljava/util/Optional;'
-//             },
-//             'transformer': (method: MethodNode) => {
-//                 const target = new InsnList();
-//                 target.add(new VarInsnNode(Opcodes.ALOAD, 0)); // this
-//                 target.add(new VarInsnNode(Opcodes.ALOAD, 1)); // destination
-//                 target.add(new VarInsnNode(Opcodes.ALOAD, 2)); // findFrom
-//                 target.add(new VarInsnNode(Opcodes.ILOAD, 3)); // isToNether
-//                 target.add(new VarInsnNode(Opcodes.ALOAD, 4)); // worldBorder
-//                 target.add(new MethodInsnNode(Opcodes.INVOKESTATIC,
-//                     'de/melanx/skyblockbuilder/util/CoreUtil',
-//                     'getExitPortal', '(Lnet/minecraft/server/level/ServerPlayer;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;ZLnet/minecraft/world/level/border/WorldBorder;)Ljava/util/Optional;'
-//                 ));
-//                 target.add(new InsnNode(Opcodes.ARETURN));
-//
-//                 for (let i = 0; i < method.instructions.size(); i++) {
-//                     const node = method.instructions.get(i) as AbstractInsnNode;
-//                     if (node.getOpcode() == Opcodes.IFEQ) {
-//                         ASMAPI.log("INFO", "" + node.getNext(), [])
-//                         ASMAPI.log("INFO", "asdgjhasgdjhsagd", [])
-//                         const next = node.getNext() as LabelNode;
-//                         method.instructions.insert(next, target);
-//                         return method;
-//                     }
-//                 }
-//
-//                 throw new Error('Failed to patch WorldPreset.class');
-//             }
-//         }
-//     }
-// }
 function initializeCoreMod() {
     return {
         'redirect_exit_portal': {
@@ -50,7 +12,7 @@ function initializeCoreMod() {
             },
             'transformer': function (method) {
                 var target = new coremods_1.InsnList();
-                // target.add(new VarInsnNode(Opcodes.ALOAD, 0)); // this
+                // target.add(new VarInsnNode(Opcodes.ALOAD, 0)); // this already loaded
                 target.add(new coremods_1.VarInsnNode(coremods_1.Opcodes.ALOAD, 1)); // destination
                 target.add(new coremods_1.VarInsnNode(coremods_1.Opcodes.ALOAD, 2)); // findFrom
                 target.add(new coremods_1.VarInsnNode(coremods_1.Opcodes.ILOAD, 3)); // isToNether
@@ -64,12 +26,11 @@ function initializeCoreMod() {
                         if (methodNode.owner == 'net/minecraft/server/level/ServerPlayer'
                             && methodNode.name == coremods_1.ASMAPI.mapMethod('m_9236_')) {
                             method.instructions.insertBefore(methodNode, target);
-                            //                          method.instructions.remove(methodNode);
                             return method;
                         }
                     }
                 }
-                throw new Error('Failed to patch WorldPreset.class');
+                throw new Error('Failed to patch ServerPlayer.class');
             }
         }
     };
--- src/coremods/coremods/exit_portal.ts
+++ src/coremods/coremods/exit_portal.ts
@@ -3,51 +3,12 @@ import {
     ASMAPI,
     CoreMods,
     InsnList,
-    InsnNode, LabelNode,
+    InsnNode,
     MethodInsnNode,
     MethodNode,
     Opcodes,
     VarInsnNode
 } from "coremods";
-
-// function initializeCoreMod(): CoreMods {
-//     return {
-//         'redirect_exit_portal': {
-//             'target': {
-//                 'type': 'METHOD',
-//                 'class': 'net.minecraft.server.level.ServerPlayer',
-//                 'methodName': 'm_183318_',
-//                 'methodDesc': '(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;ZLnet/minecraft/world/level/border/WorldBorder;)Ljava/util/Optional;'
-//             },
-//             'transformer': (method: MethodNode) => {
-//                 const target = new InsnList();
-//                 target.add(new VarInsnNode(Opcodes.ALOAD, 0)); // this
-//                 target.add(new VarInsnNode(Opcodes.ALOAD, 1)); // destination
-//                 target.add(new VarInsnNode(Opcodes.ALOAD, 2)); // findFrom
-//                 target.add(new VarInsnNode(Opcodes.ILOAD, 3)); // isToNether
-//                 target.add(new VarInsnNode(Opcodes.ALOAD, 4)); // worldBorder
-//                 target.add(new MethodInsnNode(Opcodes.INVOKESTATIC,
-//                     'de/melanx/skyblockbuilder/util/CoreUtil',
-//                     'getExitPortal', '(Lnet/minecraft/server/level/ServerPlayer;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;ZLnet/minecraft/world/level/border/WorldBorder;)Ljava/util/Optional;'
-//                 ));
-//                 target.add(new InsnNode(Opcodes.ARETURN));
-//
-//                 for (let i = 0; i < method.instructions.size(); i++) {
-//                     const node = method.instructions.get(i) as AbstractInsnNode;
-//                     if (node.getOpcode() == Opcodes.IFEQ) {
-//                         ASMAPI.log("INFO", "" + node.getNext(), [])
-//                         ASMAPI.log("INFO", "asdgjhasgdjhsagd", [])
-//                         const next = node.getNext() as LabelNode;
-//                         method.instructions.insert(next, target);
-//                         return method;
-//                     }
-//                 }
-//
-//                 throw new Error('Failed to patch WorldPreset.class');
-//             }
-//         }
-//     }
-// }
 
 function initializeCoreMod(): CoreMods {
     return {
@@ -60,7 +21,7 @@ function initializeCoreMod(): CoreMods {
             },
             'transformer': (method: MethodNode) => {
                 const target = new InsnList();
-                // target.add(new VarInsnNode(Opcodes.ALOAD, 0)); // this
+                // target.add(new VarInsnNode(Opcodes.ALOAD, 0)); // this already loaded
                 target.add(new VarInsnNode(Opcodes.ALOAD, 1)); // destination
                 target.add(new VarInsnNode(Opcodes.ALOAD, 2)); // findFrom
                 target.add(new VarInsnNode(Opcodes.ILOAD, 3)); // isToNether
@@ -78,13 +39,12 @@ function initializeCoreMod(): CoreMods {
                         if (methodNode.owner == 'net/minecraft/server/level/ServerPlayer'
                             && methodNode.name == ASMAPI.mapMethod('m_9236_')) {
                             method.instructions.insertBefore(methodNode, target);
-//                          method.instructions.remove(methodNode);
                             return method;
                         }
                     }
                 }
 
-                throw new Error('Failed to patch WorldPreset.class');
+                throw new Error('Failed to patch ServerPlayer.class');
             }
         }
     }
