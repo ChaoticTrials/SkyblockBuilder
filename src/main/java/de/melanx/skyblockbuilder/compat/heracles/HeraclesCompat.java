9740dba934b9e690751872024a190c5fc2909a9d
MelanX <info@melanx.de>
2024-06-05 19:59:13 +0200

    add default title for heracles task

--- src/main/java/de/melanx/skyblockbuilder/compat/heracles/HeraclesCompat.java
+++ src/main/java/de/melanx/skyblockbuilder/compat/heracles/HeraclesCompat.java
@@ -3,6 +3,9 @@ package de.melanx.skyblockbuilder.compat.heracles;
 import earth.terrarium.heracles.api.client.settings.Settings;
 import earth.terrarium.heracles.api.tasks.QuestTaskDisplayFormatter;
 import earth.terrarium.heracles.api.tasks.client.QuestTaskWidgets;
+import earth.terrarium.heracles.api.tasks.client.display.TaskTitleFormatter;
+import earth.terrarium.heracles.api.tasks.client.display.TaskTitleFormatters;
+import net.minecraft.network.chat.Component;
 import net.minecraftforge.common.MinecraftForge;
 
 public class HeraclesCompat {
@@ -14,6 +17,13 @@ public class HeraclesCompat {
         Settings.register(SpreadLocationTask.TYPE, SpreadTaskSettings.INSTANCE);
         QuestTaskDisplayFormatter.register(SpreadLocationTask.TYPE, ((progress, task) -> String.format("%d/%d", task.storage().read(progress.progress()) ? 1 : 0, 1)));
         QuestTaskWidgets.registerSimple(SpreadLocationTask.TYPE, SpreadTaskWidget::new);
+        TaskTitleFormatter.register(SpreadLocationTask.TYPE, task -> {
+            if (task.predicate().getSpreads().size() > 1) {
+                return Component.translatable(TaskTitleFormatters.toTranslationKey(task, false), task.predicate().getSpreads().size());
+            } else {
+                return Component.translatable(TaskTitleFormatters.toTranslationKey(task, true));
+            }
+        });
         MinecraftForge.EVENT_BUS.register(new HeraclesEventHandler());
     }
 }
