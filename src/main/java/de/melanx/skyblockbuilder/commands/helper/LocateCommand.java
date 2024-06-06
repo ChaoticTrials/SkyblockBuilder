43d3844bb9e51db83f857329c4011f7e64295f96
MelanX <info@melanx.de>
2024-06-06 16:06:44 +0200

    change locate command to show all spreads if none specified

--- src/main/java/de/melanx/skyblockbuilder/commands/helper/LocateCommand.java
+++ src/main/java/de/melanx/skyblockbuilder/commands/helper/LocateCommand.java
@@ -23,37 +23,67 @@ public class LocateCommand {
                 .requires(source -> source.hasPermission(2))
                 .then(Commands.literal("spread")
                         .then(Commands.argument("team", StringArgumentType.string()).suggests(Suggestions.ALL_TEAMS)
+                                .executes(LocateCommand::locateAllSpreads)
                                 .then(Commands.argument("spread", StringArgumentType.string()).suggests(Suggestions.SPREADS_FOR_TEAM)
                                         .executes(LocateCommand::locateSpread))));
     }
 
     private static int locateSpread(CommandContext<CommandSourceStack> context) {
-        String teamName = CommandUtil.getArgumentOrDefault(context, "team", String.class, "Spawn");
         String spreadName = CommandUtil.getArgumentOrDefault(context, "spread", String.class, "Spread");
-        ServerLevel level = context.getSource().getLevel();
-        SkyblockSavedData data = SkyblockSavedData.get(level);
 
-        Team team = data.getTeam(teamName);
-        if (team == null || team.isSpawn()) {
-            context.getSource().sendFailure(Component.translatable("skyblockbuilder.command.error.team_not_exist"));
+        Team team = LocateCommand.getTeam(context);
+        if (team == null) {
             return 0;
         }
 
-        Set<Team.PlacedSpread> placedSpreads = team.getPlacedSpreads(spreadName);
-        if (spreadName == null || placedSpreads.isEmpty()) {
+        if (team.getPlacedSpreads(spreadName).isEmpty()) {
             context.getSource().sendFailure(Component.translatable("skyblockbuilder.command.error.spread_not_exist"));
             return 0;
         }
 
-        context.getSource().sendSuccess(() -> {
+        LocateCommand.sendLocations(context.getSource(), team, spreadName);
+        return 1;
+    }
+
+    private static int locateAllSpreads(CommandContext<CommandSourceStack> context) {
+        Team team = LocateCommand.getTeam(context);
+        if (team == null) {
+            return 0;
+        }
+
+        Set<String> spreadNames = team.getAllSpreadNames();
+        if (spreadNames.isEmpty()) {
+            context.getSource().sendFailure(Component.translatable("skyblockbuilder.command.error.no_spreads"));
+            return 0;
+        }
+
+        spreadNames.forEach(spreadName -> LocateCommand.sendLocations(context.getSource(), team, spreadName));
+
+        return spreadNames.size();
+    }
+
+    private static void sendLocations(CommandSourceStack source, Team team, String spreadName) {
+        source.sendSuccess(() -> {
             MutableComponent msg = Component.translatable("skyblockbuilder.command.success.located_spread", spreadName);
-            for (Team.PlacedSpread spread : placedSpreads) {
+            for (Team.PlacedSpread spread : team.getPlacedSpreads(spreadName)) {
                 msg.append("\n- ");
                 msg.append(RandomUtility.getFormattedPos(spread.pos()));
             }
 
             return msg;
         }, true);
-        return 1;
+    }
+
+    private static Team getTeam(CommandContext<CommandSourceStack> context) {
+        String teamName = CommandUtil.getArgumentOrDefault(context, "team", String.class, "Spawn");
+        ServerLevel level = context.getSource().getLevel();
+        SkyblockSavedData data = SkyblockSavedData.get(level);
+
+        Team team = data.getTeam(teamName);
+        if (team == null || team.isSpawn()) {
+            context.getSource().sendFailure(Component.translatable("skyblockbuilder.command.error.team_not_exist"));
+        }
+
+        return team;
     }
 }
