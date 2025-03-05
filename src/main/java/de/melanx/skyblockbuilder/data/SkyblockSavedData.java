473b3c5057aba5dbca78fdde6f3aee958cde26fc
MelanX <info@melanx.de>
2025-03-05 15:32:12 +0100

    Only give starting inventory when not spawn

--- src/main/java/de/melanx/skyblockbuilder/data/SkyblockSavedData.java
+++ src/main/java/de/melanx/skyblockbuilder/data/SkyblockSavedData.java
@@ -248,7 +248,7 @@ public class SkyblockSavedData extends SavedData {
         }
 
         ServerLevel level = team.getLevel();
-        if (level != null && !this.getOrCreateMetaInfo(player).getPreviousTeamIds().contains(team.getId())) {
+        if (!team.isSpawn() && level != null && !this.getOrCreateMetaInfo(player).getPreviousTeamIds().contains(team.getId())) {
             ServerPlayer onlinePlayer = level.getServer().getPlayerList().getPlayer(player);
             if (onlinePlayer != null && (TemplatesConfig.mainSpawnIsland.isEmpty() || !team.isSpawn())) {
                 RandomUtility.setStartInventory(onlinePlayer);
