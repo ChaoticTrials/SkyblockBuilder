b6a6074f7a6e928df22de4543519680772cc59f7
MelanX <info@melanx.de>
2025-03-05 19:34:30 +0100

    Update link to starting inventory documentation

--- src/main/java/de/melanx/skyblockbuilder/util/SkyPaths.java
+++ src/main/java/de/melanx/skyblockbuilder/util/SkyPaths.java
@@ -129,7 +129,7 @@ public class SkyPaths {
 
         BufferedWriter w = Files.newBufferedWriter(ITEMS_FILE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
         w.write("// See here for more information:\n");
-        w.write("// https://wiki.chaotictrials.de/swl/skyblock-builder\n"); // todo correct link
+        w.write("// https://wiki.chaotictrials.de/docs/wiki/skyblock-builder/packdev/configs/starting-inventory\n");
         w.write("// If this page isn't available, go to the project page (where you downloaded the file), and click on the wiki\n");
         w.write(SkyblockBuilder.PRETTY_GSON.toJson(object));
         w.close();
