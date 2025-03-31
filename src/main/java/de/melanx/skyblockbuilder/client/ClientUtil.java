655da51e6d6fd1f0cbf66d5a03499b346221d526
MelanX <info@melanx.de>
2025-03-31 17:09:29 +0200

    Fix server crash on startup

--- src/main/java/de/melanx/skyblockbuilder/client/ClientUtil.java
+++ src/main/java/de/melanx/skyblockbuilder/client/ClientUtil.java
@@ -4,11 +4,18 @@ import de.melanx.skyblockbuilder.client.screens.StructureSaverScreen;
 import de.melanx.skyblockbuilder.util.SkyComponents;
 import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.network.chat.CommonComponents;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
+import net.neoforged.fml.loading.FMLPaths;
+import org.moddingx.libx.screen.text.ComponentLayout;
+import org.moddingx.libx.screen.text.TextScreen;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 public class ClientUtil {
 
@@ -24,5 +31,29 @@ public class ClientUtil {
 
     public static void openItemScreen(ItemStack stack) {
         Minecraft.getInstance().setScreen(new StructureSaverScreen(stack));
+    }
+
+    public static void openErrorScreen(Path configPath, String reason) {
+        Minecraft.getInstance().setScreen(new TextScreen(ComponentLayout.simple(
+                SkyComponents.SCREEN_ERROR_TITLE,
+                List.of(Component.literal("Failed to overwrite config " + FMLPaths.GAMEDIR.get().relativize(configPath)), Component.literal(reason))
+        ), 230) {
+
+            @Override
+            protected void init() {
+                super.init();
+
+                this.addRenderableWidget(Button.builder(CommonComponents.GUI_OK, button -> Minecraft.getInstance().setScreen(null))
+                        .pos(this.width / 2 - 115, this.height - 27)
+                        .width(110)
+                        .build());
+                this.addRenderableWidget(Button.builder(Component.literal("Open Config"), button -> {
+                            Util.getPlatform().openFile(configPath.toFile());
+                        })
+                        .pos(this.width / 2 + 5, this.height - 27)
+                        .width(110)
+                        .build());
+            }
+        });
     }
 }
--- src/main/java/de/melanx/skyblockbuilder/network/ExportErrorHandler.java
+++ src/main/java/de/melanx/skyblockbuilder/network/ExportErrorHandler.java
@@ -1,27 +1,18 @@
 package de.melanx.skyblockbuilder.network;
 
 import de.melanx.skyblockbuilder.SkyblockBuilder;
-import de.melanx.skyblockbuilder.util.SkyComponents;
-import net.minecraft.Util;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.components.Button;
+import de.melanx.skyblockbuilder.client.ClientUtil;
 import net.minecraft.network.RegistryFriendlyByteBuf;
-import net.minecraft.network.chat.CommonComponents;
-import net.minecraft.network.chat.Component;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.PacketFlow;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
-import net.neoforged.fml.loading.FMLPaths;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 import net.neoforged.neoforge.network.registration.HandlerThread;
 import org.moddingx.libx.network.PacketHandler;
-import org.moddingx.libx.screen.text.ComponentLayout;
-import org.moddingx.libx.screen.text.TextScreen;
 
 import javax.annotation.Nonnull;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 
 public class ExportErrorHandler extends PacketHandler<ExportErrorHandler.Message> {
 
@@ -34,27 +25,7 @@ public class ExportErrorHandler extends PacketHandler<ExportErrorHandler.Message
     @Override
     public void handle(Message msg, IPayloadContext ctx) {
         Path configPath = Paths.get(msg.configPath);
-        Minecraft.getInstance().setScreen(new TextScreen(ComponentLayout.simple(
-                SkyComponents.SCREEN_ERROR_TITLE,
-                List.of(Component.literal("Failed to overwrite config " + FMLPaths.GAMEDIR.get().relativize(configPath)), Component.literal(msg.reason))
-        ), 230) {
-
-            @Override
-            protected void init() {
-                super.init();
-
-                this.addRenderableWidget(Button.builder(CommonComponents.GUI_OK, button -> Minecraft.getInstance().setScreen(null))
-                        .pos(this.width / 2 - 115, this.height - 27)
-                        .width(110)
-                        .build());
-                this.addRenderableWidget(Button.builder(Component.literal("Open Config"), button -> {
-                            Util.getPlatform().openFile(configPath.toFile());
-                        })
-                        .pos(this.width / 2 + 5, this.height - 27)
-                        .width(110)
-                        .build());
-            }
-        });
+        ClientUtil.openErrorScreen(configPath, msg.reason);
     }
 
     public record Message(String configPath, String reason) implements CustomPacketPayload {
