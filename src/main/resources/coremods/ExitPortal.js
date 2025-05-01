bac7cf1fbaee455488edf129809d89dcbe1846ce
MelanX <info@melanx.de>
2025-05-01 11:26:02 +0200

    [meta] Move from TS to JS coremods

--- src/main/resources/coremods/ExitPortal.js
+++ src/main/resources/coremods/ExitPortal.js
@@ -1,16 +1,11 @@
-import {
-    AbstractInsnNode,
-    ASMAPI,
-    CoreMods,
-    InsnList,
-    InsnNode,
-    MethodInsnNode,
-    MethodNode,
-    Opcodes,
-    VarInsnNode
-} from "coremods";
+function initializeCoreMod() {
+    var ASMAPI = Java.type('net.minecraftforge.coremod.api.ASMAPI');
+    var Opcodes = Java.type('org.objectweb.asm.Opcodes');
+    var InsnList = Java.type('org.objectweb.asm.tree.InsnList');
+    var InsnNode = Java.type('org.objectweb.asm.tree.InsnNode');
+    var VarInsnNode = Java.type('org.objectweb.asm.tree.VarInsnNode');
+    var MethodInsnNode = Java.type('org.objectweb.asm.tree.MethodInsnNode');
 
-function initializeCoreMod(): CoreMods {
     return {
         'redirect_exit_portal': {
             'target': {
@@ -19,23 +14,19 @@ function initializeCoreMod(): CoreMods {
                 'methodName': 'm_183318_',
                 'methodDesc': '(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;ZLnet/minecraft/world/level/border/WorldBorder;)Ljava/util/Optional;'
             },
-            'transformer': (method: MethodNode) => {
-                const target = new InsnList();
+            'transformer': function (method) {
+                var target = new InsnList();
                 // target.add(new VarInsnNode(Opcodes.ALOAD, 0)); // this already loaded
                 target.add(new VarInsnNode(Opcodes.ALOAD, 1)); // destination
                 target.add(new VarInsnNode(Opcodes.ALOAD, 2)); // findFrom
                 target.add(new VarInsnNode(Opcodes.ILOAD, 3)); // isToNether
                 target.add(new VarInsnNode(Opcodes.ALOAD, 4)); // worldBorder
-                target.add(new MethodInsnNode(Opcodes.INVOKESTATIC,
-                    'de/melanx/skyblockbuilder/util/CoreUtil',
-                    'getExitPortal', '(Lnet/minecraft/server/level/ServerPlayer;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;ZLnet/minecraft/world/level/border/WorldBorder;)Ljava/util/Optional;'
-                ));
+                target.add(new MethodInsnNode(Opcodes.INVOKESTATIC, 'de/melanx/skyblockbuilder/util/CoreUtil', 'getExitPortal', '(Lnet/minecraft/server/level/ServerPlayer;Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/core/BlockPos;ZLnet/minecraft/world/level/border/WorldBorder;)Ljava/util/Optional;'));
                 target.add(new InsnNode(Opcodes.ARETURN));
-
-                for (let i = 0; i < method.instructions.size(); i++) {
-                    const node = method.instructions.get(i) as AbstractInsnNode;
+                for (var i = 0; i < method.instructions.size(); i++) {
+                    var node = method.instructions.get(i);
                     if (node.getOpcode() == Opcodes.INVOKEVIRTUAL) {
-                        const methodNode = node as MethodInsnNode;
+                        var methodNode = node;
                         if (methodNode.owner == 'net/minecraft/server/level/ServerPlayer'
                             && methodNode.name == ASMAPI.mapMethod('m_9236_')) {
                             method.instructions.insertBefore(methodNode, target);
@@ -43,9 +34,8 @@ function initializeCoreMod(): CoreMods {
                         }
                     }
                 }
-
                 throw new Error('Failed to patch ServerPlayer.class');
             }
         }
-    }
+    };
 }
--- src/main/resources/coremods/WorldPresetCodec.js
+++ src/main/resources/coremods/WorldPresetCodec.js
@@ -1,7 +1,9 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var coremods_1 = require("coremods");
 function initializeCoreMod() {
+    var ASMAPI = Java.type('net.minecraftforge.coremod.api.ASMAPI');
+    var Opcodes = Java.type('org.objectweb.asm.Opcodes');
+    var InsnList = Java.type('org.objectweb.asm.tree.InsnList');
+    var MethodInsnNode = Java.type('org.objectweb.asm.tree.MethodInsnNode');
+
     return {
         'modify_codec': {
             'target': {
@@ -11,13 +13,13 @@ function initializeCoreMod() {
                 'methodDesc': '()V'
             },
             'transformer': function (method) {
-                var target = new coremods_1.InsnList();
-                target.add(new coremods_1.MethodInsnNode(coremods_1.Opcodes.INVOKESTATIC, 'de/melanx/skyblockbuilder/util/CoreUtil', 'augmentWorldPresetCodec', '(Lcom/mojang/serialization/Codec;)Lcom/mojang/serialization/Codec;'));
+                var target = new InsnList();
+                target.add(new MethodInsnNode(Opcodes.INVOKESTATIC, 'de/melanx/skyblockbuilder/util/CoreUtil', 'augmentWorldPresetCodec', '(Lcom/mojang/serialization/Codec;)Lcom/mojang/serialization/Codec;'));
                 for (var i = 0; i < method.instructions.size(); i++) {
                     var node = method.instructions.get(i);
-                    if (node.getOpcode() == coremods_1.Opcodes.PUTSTATIC) {
+                    if (node.getOpcode() == Opcodes.PUTSTATIC) {
                         var fieldNode = node;
-                        if (fieldNode.owner == 'net/minecraft/world/level/levelgen/presets/WorldPreset' && fieldNode.name == coremods_1.ASMAPI.mapField('f_226414_')) {
+                        if (fieldNode.owner == 'net/minecraft/world/level/levelgen/presets/WorldPreset' && fieldNode.name == ASMAPI.mapField('f_226414_')) {
                             method.instructions.insertBefore(fieldNode, target);
                             return method;
                         }
